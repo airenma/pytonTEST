187f90540cf6e0561dca89cb14be6b4696188170
airenma <airenma@gmail.com>
2016-05-31 22:36:41 +0300

    build system gulp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ',' ']; 
+var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; 
 //функция, сдвигающая алфавит на количество букв shift
 
 function shiftAlphabet(shift) {
